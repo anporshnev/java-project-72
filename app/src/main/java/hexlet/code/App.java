2748d5daeb78925da4318dcee1437897ebc1ed5f
AndrewPorshnev <andrew.porshnev@yandex.ru>
2024-01-09 18:22:24 +0300

    Создает приложение Javaline

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,24 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.parseInt(port);
+    }
+
+
+    public static Javalin getApp() {
+        var app = Javalin.create(conf -> conf.plugins.enableDevLogging());
+
+        app.get("/", ctx -> ctx.result("Hello, World!"));
+
+
+        return app;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Javalin app = getApp();
+        app.start(getPort());
     }
 }
