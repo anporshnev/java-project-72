1e1e025376776ce692e98febe0ba534b8a738e98
AndrewPorshnev <andrew.porshnev@yandex.ru>
2024-01-10 15:25:20 +0300

    Добавляет поддержку и подключение к БД

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     checkstyle
     id("com.adarshr.test-logger") version "4.0.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.freefair.lombok") version "8.4"
 }
 
 application {
@@ -20,6 +21,9 @@ repositories {
 dependencies {
     implementation ("io.javalin:javalin:5.6.3")
     implementation ("org.slf4j:slf4j-simple:2.0.9")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("com.h2database:h2:2.2.224")
+    implementation("org.postgresql:postgresql:42.7.1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,19 +1,35 @@
 package hexlet.code;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class App {
+
+    private static final String DEFAULT_PORT = "7070";
+    private static final String DEFAULT_DB = "jdbc:h2:mem:hexlet_project;DB_CLOSE_DELAY=-1;";
+
     private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "7070");
+        String port = System.getenv().getOrDefault("PORT", DEFAULT_PORT);
         return Integer.parseInt(port);
     }
+    private static String getDatabase() {
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL", DEFAULT_DB);
 
+    }
 
     public static Javalin getApp() {
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(getDatabase());
+
+        BaseRepository.dataSource = new HikariDataSource(hikariConfig);
+
         var app = Javalin.create(conf -> conf.plugins.enableDevLogging());
 
         app.get("/", ctx -> ctx.result("Hello, World!"));
-
 
         return app;
     }
