c6152ce5807fd19e13b647619bd96d613e52ddf4
AndrewPorshnev <andrew.porshnev@yandex.ru>
2024-01-11 18:39:07 +0300

    Добавляет алерты

--- app/src/main/java/hexlet/code/dto/BasePage.java
+++ app/src/main/java/hexlet/code/dto/BasePage.java
@@ -1,11 +1,13 @@
 package hexlet.code.dto;
 
 import hexlet.code.util.AlertsType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class BasePage {
     private String alertMessage;
     private AlertsType typeAlert;
