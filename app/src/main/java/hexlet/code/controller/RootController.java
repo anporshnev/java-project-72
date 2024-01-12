47bd13f79bb8ce6527ffb3c89599b0db01286a10
AndrewPorshnev <andrew.porshnev@yandex.ru>
2024-01-12 12:39:03 +0300

    Реализует алерты в шаблонах

--- app/src/main/java/hexlet/code/controller/RootController.java
+++ app/src/main/java/hexlet/code/controller/RootController.java
@@ -1,9 +1,17 @@
 package hexlet.code.controller;
 
+import hexlet.code.dto.BasePage;
 import io.javalin.http.Context;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Collections;
+
+@Slf4j
 public class RootController {
     public static void index(Context ctx) {
-        ctx.render("index.jte");
+        var page = new BasePage();
+        page.setAlertMessage(ctx.consumeSessionAttribute("alertMessage"));
+        page.setTypeAlert(ctx.consumeSessionAttribute("typeAlert"));
+        ctx.render("index.jte", Collections.singletonMap("page", page));
     }
 }
