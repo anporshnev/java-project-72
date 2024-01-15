d7aadf169bbc99c684702b63652aca701e16f17e
AndrewPorshnev <andrew.porshnev@yandex.ru>
2024-01-15 17:08:39 +0300

    Добавляет метод форматирования даты

---
+++ app/src/main/java/hexlet/code/util/DateFormatter.java
@@ -0,0 +1,12 @@
+package hexlet.code.util;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class DateFormatter {
+    public static String formatDate(Instant date, String pattern) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern).withZone(ZoneId.systemDefault());;
+        return formatter.format(date);
+    }
+}
